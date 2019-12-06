4482fb618a364e85bcebb6dc04e0c9383e3c324b
marcelovqvd <marcelovqvd@gmail.com>
2019-12-06 16:37:11 -0300

    loader de survivors

---
+++ src/database/index.js
@@ -0,0 +1,21 @@
+import Sequelize from 'sequelize';
+
+import Survivor from '../app/models/Survivor';
+
+import databaseConfig from '../config/database';
+
+const models = [Survivor];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+
+    models.map(model => model.init(this.connection));
+  }
+}
+
+export default new Database();
